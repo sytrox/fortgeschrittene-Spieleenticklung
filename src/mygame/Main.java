58d6ab0b9a3a5924abc21a2e320a73591cc1a7d1
sytrox <david.biskupek@gmail.com>
2015-03-17 00:12:03 +0100

    init

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -18,6 +18,7 @@ import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Quad;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.system.AppSettings;
+
 import com.jme3.texture.FrameBuffer;
 import com.jme3.texture.Image;
 import com.jme3.texture.Texture;
